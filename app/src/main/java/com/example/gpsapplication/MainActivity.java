aba778b60b9dafbf6d8ef6cbe4b94b9dc6266f82
nikki <nikkimeadows1@gmail.com>
2020-03-22 17:23:17 +1300

    data is now being saved in a text file on the android device.

--- app/src/main/java/com/example/gpsapplication/MainActivity.java
+++ app/src/main/java/com/example/gpsapplication/MainActivity.java
@@ -1,26 +1,33 @@
 package com.example.gpsapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-
     Button butGetLoc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         butGetLoc= (Button) findViewById(R.id.butGetLoc);
@@ -29,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         //listener for the button
         butGetLoc.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View view) {
             //use methods from the GPS tracker class
@@ -40,33 +48,34 @@ public class MainActivity extends AppCompatActivity {
                     Double lat=l.getLatitude();
                     Double lon=l.getLongitude();
                     Toast.makeText(getApplicationContext(), "LAT: "+lat+ " \n LON: "+lon,Toast.LENGTH_LONG ).show();
+                    Toast.makeText(getApplicationContext(), "Values Saved in Txt File",Toast.LENGTH_LONG ).show();
+                    String textToSaveLat = "Latitude: " + Double.toString( lat) ;
+                    String textToSaveLon = "\nLongitude: "+ Double.toString( lon);
 
+                File root = android.os.Environment.getExternalStorageDirectory();
 
-                        String textToSaveLat = "Latitude: " + Double.toString( lat) ;
-                        String textToSaveLon = "\nLongitude: "+ Double.toString( lon);
+                File dir = new File (root.getAbsolutePath() + "/download");
+                dir.mkdirs();
+                File file = new File(dir, "longitudelatitude.txt");
 
+                try {
+                    FileOutputStream f = new FileOutputStream(file);
+                    PrintWriter pw = new PrintWriter(f);
+                    f.write(textToSaveLat.getBytes());
+                    f.write(textToSaveLon.getBytes());
+                    f.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
 
-                        try {
-                            FileOutputStream fileOutputStream = openFileOutput("LatitudeLongitude.txt", MODE_PRIVATE);
-                            fileOutputStream.write(textToSaveLat.getBytes());
-                            fileOutputStream.write(textToSaveLon.getBytes());
-                            fileOutputStream.close();
-
-                            Toast.makeText(getApplicationContext(), "Text Saved", Toast.LENGTH_SHORT).show();
-
-
-                           // inputField.setText("");
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+
+            }
 
             }
         });
     }
-
-
 }
+
+
